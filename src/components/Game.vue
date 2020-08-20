7216b10804ccb75d3fa43b846e0b26f048ffd9f3
NatthatidaS <68525489+NatthatidaS@users.noreply.github.com>
2020-08-20 15:59:48 +0700

    edit

---
+++ src/components/Game.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>
+      HP Player{{ randomNo(1,7) }}
+      {{player[randomNo(1,7)-1].name }}
+    </p>
+    +รูป
+    <hr />
+    name = {{ aPlayer.name }}
+    hp={{ aPlayer.hp }}
+    
+  </div>
+</template>
